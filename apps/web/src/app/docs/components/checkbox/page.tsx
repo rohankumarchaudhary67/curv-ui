8ee701448bd4be6f8d410f4849dce9f265f4ec0b
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2025-11-01 15:35:19 +0530

    feat: add checkbox component

---
+++ apps/web/src/app/docs/components/checkbox/page.tsx
@@ -0,0 +1,50 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import CodeBlock from "@/components/code-block";
+import NavigationButtons from "@/components/buttons/navigation-button";
+
+export default function ButtonDocs() {
+    return (
+        <div className="max-w-3xl ml-0 md:ml-24 p-4 pt-12 md:p-8 text-white bg-black min-h-screen">
+            <h1 className="text-4xl font-bold mb-3">Button</h1>
+            <p className="text-neutral-400 mb-6">
+                Displays a button or a component that looks like a button.
+            </p>
+
+            <section className="mb-6">
+                <div className="border border-gray-700/50 rounded-lg md:py-48 py-24 flex justify-center items-center flex-col space-y-4">
+                    <div>
+                        <Checkbox />{" "}
+                        <span className="pl-2">
+                            Accept terms and conditions
+                        </span>
+                    </div>
+                </div>
+            </section>
+
+            <section className="mb-12">
+                <h2 className="text-2xl font-bold mb-4">Installation</h2>
+                <CodeBlock code="npx curv-ui add checkbox" language="npm" />
+            </section>
+
+            <section className="mb-12">
+                <h2 className="text-2xl font-bold mb-4">Usage</h2>
+                <CodeBlock
+                    code={`import { Checkbox } from "@/components/ui/checkbox";`}
+                    language="ts"
+                />
+                <div className="h-4" />
+                <CodeBlock
+                    code={`<Checkbox /> <span className="pl-2">Accept terms and conditions</span>`}
+                    language="tsx"
+                />
+            </section>
+
+            <NavigationButtons
+                previousHref="/docs/components/button"
+                previousLabel="Button"
+                nextHref="/docs/components/input"
+                nextLabel="Input"
+            />
+        </div>
+    );
+}
--- curv-cli/src/dependencies.ts
+++ curv-cli/src/dependencies.ts
@@ -1,6 +1,11 @@
 const dependencies = {
     button: ["@radix-ui/react-slot", "class-variance-authority"],
     alert: ["class-variance-authority", "lucide-react"],
+    checkbox: [
+        "@radix-ui/react-checkbox",
+        "lucide-react",
+        "class-variance-authority",
+    ],
 };
 
 export default dependencies;
